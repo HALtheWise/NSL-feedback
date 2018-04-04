de1b97ed592acc5390d6a317b5cc9fd875c61870
Eric Miller <eric@legoaces.org>
2018-04-04 12:16:28 -0400

    Added OpenCV display code

---
+++ display.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+
+def main():
+    cv2.namedWindow('feedback')
+    cv2.moveWindow('feedback', 100, 100)
+
+    while True:
+        ret, frame = cap.read()
+
+        cv2.imshow('feedback', frame)
+
+        key = chr(cv2.waitKey(1) & 0xFF)
+        if key == 'q':
+            # Quit if the "q" key is pressed over the window
+            break
+        if key == 's':
+            # TODO: save the image
+            pass
+
+
+if __name__ == '__main__':
+    main()
