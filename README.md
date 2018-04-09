d220159d9846a6666c4d3e1c743f671bb6e77081
tatianaanthony <tatiana.anthony@students.olin.edu>
2018-04-09 15:22:28 -0400

    Adding guide

--- README.md
+++ README.md
@@ -6,3 +6,25 @@ To run:
 > pipenv shell 
 > python display.py
 
+
+Desired Functionality (**BOLD** required by Thursday, *Italics* desired by Thursday):
+
+- [] Simulate camera settings
+	- [] **Brightness**
+	- [] *Contrast*
+	- [] Zoom
+	- [] Translation
+	- [] Rotation
+	- [] Keystone
+	- [] Frame Rate
+	- [] Time Delay
+- [] Live editing camera settings
+	- [] *Sliders below window for each setting*
+- [] Overlay Effects
+	- [] **4 crosshairs on a circle (custom opencv fine)**
+	- [] Center Dot (custom opencv fine)
+	- [] Arbitrary Initial Condition Images (overlaying PNG)
+- [] **Save images** 
+- [] Save Video
+
+
--- display.py
+++ display.py
@@ -9,8 +9,15 @@ def main():
     cv2.namedWindow('feedback')
     cv2.moveWindow('feedback', 600, 600)
 
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) 
+    center = (int(width/2),int(height/2))
+    markerrad = int(height/4)
+    
+    print(width)
+    print(height)
     downsample = False
-    guide = False
+    guide = True
     brightness = 50
     # cv2.createTrackbar("brightness", 'feedback', 0, 100)
 
@@ -19,16 +26,20 @@ def main():
 
         if downsample:
             # TODO: this
+            
             pass
 
         if guide:
             # TODO: this
+            cv2.circle(frame, center, markerrad, (255,0,0), 2)
+            cv2.drawMarker(frame, (center[0]+markerrad, center[1]),(0,0,255),cv2.MARKER_CROSS,20,2,cv2.FILLED)
             pass
 
         if brightness != 50:
             # TODO: this
             pass
 
+        
         key = chr(cv2.waitKey(1) & 0xFF)
         if key == 'q':
             # Quit if the "q" key is pressed over the window
