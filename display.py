2d8761f8e380078118a10fc7b58e2a901221512f
Eric Miller <eric@legoaces.org>
2018-04-11 12:20:47 -0400

    Fixed merge problems

--- display.py
+++ display.py
@@ -15,8 +15,8 @@ def main():
     windowx = 600
     windowy = 200
     cv2.moveWindow('feedback', windowx, windowy)
-    
-    #Get window values
+
+    # Get window values
 
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -30,21 +30,21 @@ def main():
         nonlocal brightness
         brightness = val
 
-    #Set up trackbars
+    # Set up trackbars
     cv2.createTrackbar("brightness", 'feedback', 0, 255, updateBrightness)
-    cv2.setTrackbarMin('brightness', 'feedback', -255) 
-    
+    cv2.setTrackbarMin('brightness', 'feedback', -255)
+
     # Set up overlay images
     overframe = cv2.imread("white.jpg")
-    overframe = cv2.resize(overframe, dsize = (int(width),int(height)));
-    
+    overframe = cv2.resize(overframe, dsize=(int(width), int(height)));
+
     # Set up flags
     downsample = False
-    guide = False
+    guide = True
     brightness = 50
     trailing_average_weights = []  # Last element is weight to give previous frame, etc...
     old_frames = []
-    overlay = True
+    overlay = False
 
     while True:
         ret, frame = cap.read()
@@ -80,10 +80,9 @@ def main():
                     frame = frame + f * w
                 frame = np.array(frame, dtype=np.uint8)
 
-        
         if overlay:
             frame = overframe
-        
+
         key = chr(cv2.waitKey(1) & 0xFF)
         if key == 'q':
             # Quit if the "q" key is pressed over the window
@@ -93,7 +92,7 @@ def main():
             savename = datetime.datetime.now().strftime("%Y-%M-%d-%H:%M")
             savename += '.png'
             print(savename)
-            cv2.imwrite(savename,frame)
+            cv2.imwrite(savename, frame)
         if key == 'r':
             downsample = not downsample
         if key == 'g':
@@ -102,26 +101,26 @@ def main():
             overlay = not overlay
         if key == 'w':
             windowy += -1
-            cv2.moveWindow(	'feedback', windowx,windowy	)
+            cv2.moveWindow('feedback', windowx, windowy)
         if key == 's':
             windowy += 1
-            cv2.moveWindow(	'feedback', windowx,windowy	)
+            cv2.moveWindow('feedback', windowx, windowy)
         if key == 'a':
             windowx += -1
-            cv2.moveWindow('feedback',windowx,windowy)
+            cv2.moveWindow('feedback', windowx, windowy)
         if key == 'd':
             windowx += 1
-            cv2.moveWindow('feedback',windowx,windowy)
+            cv2.moveWindow('feedback', windowx, windowy)
         if key == 'm':
             if trailing_average_weights:
                 trailing_average_weights = []
                 print("Trailing Averaging Disabled")
             else:
-                trailing_average_weights = [.5,0]
+                trailing_average_weights = [.5, 0]
                 print("Trailing Averaging enabled: ", trailing_average_weights)
-
 
         cv2.imshow('feedback', frame)
 
 
-if __name__ == '__main__' 
+if __name__ == '__main__':
+    main()
